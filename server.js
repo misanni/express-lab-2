a79d81431794895b290e565979c837020f94ba6e
Mary Sanni <marysanni01@gmail.com>
2022-11-07 10:34:10 -0600

    magic 8 ball

--- server.js
+++ server.js
@@ -21,8 +21,12 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
 });
 
 app.get('/magic/:question',(req, res)=> {
-    var answer = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"];
-    res.send(req.params.question)
+    function get_random (answer) {
+        return answer[Math.floor((Math.random()*answer.length))];
+      }
+res.send
+      returns = get_random(["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"])
+      res.send(` ${req.params.question}, <br>  <h1>${returns} </h1>`)
 });
 
 app.listen(3000,()=>{
